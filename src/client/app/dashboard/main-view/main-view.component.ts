2a4c1c819883750708299fc2c0470b7e4733ad87
KrashLeviathan <nathankarasch@gmail.com>
2017-02-27 23:15:26 -0600

    Packery and draggabilly functionality

--- src/client/app/dashboard/main-view/main-view.component.ts
+++ src/client/app/dashboard/main-view/main-view.component.ts
@@ -1,11 +1,10 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import $ = require('jquery');
 
-declare var packery: any;
-declare var Draggabilly: any;
-
-// Resources:
-// Packery  -
+// Packery  -  http://packery.metafizzy.co/
+declare let Packery: any;
 // Draggabilly  -  http://draggabilly.desandro.com/
+declare let Draggabilly: any;
 
 @Component({
   moduleId: module.id,
@@ -14,21 +13,21 @@ declare var Draggabilly: any;
   styleUrls: ['./main-view.component.css']
 })
 
-export class MainViewComponent {
+export class MainViewComponent implements OnInit {
   // https://www.thepolyglotdeveloper.com/2016/01/include-external-javascript-libraries-in-an-angular-2-typescript-project/
-  // packery: any;
+  packery: any;
 
   // Also got help from https://github.com/metafizzy/packery/issues/337 for persistent positioning
 
-  dataItemAttribute: 'data-item-id';
+  dataItemAttribute: string = 'data-item-id';
 
-  draggabillyOptions: {
+  draggabillyOptions: any = {
     containment: '.grid'
   };
 
-  packeryOptions: {
+  packeryOptions: any = {
     itemSelector: '.grid-item',
-    columnWidth: 100,
+    columnWidth: 150,
     initLayout: false // disable initial layout
   };
 
@@ -36,13 +35,18 @@ export class MainViewComponent {
   editModeActive: boolean = false;
   $grid: any;
 
-  constructor() {
+  static getSavedDraggedPositions(): string {
+    return localStorage.getItem('dragPositions');
+  }
+
+  ngOnInit() {
+    this.$grid = $('.grid');
     // init Packery
-    this.$grid = $('.grid').packery(this.packeryOptions);
-    let initPositions = this.getSavedDraggedPositions();
+    this.packery = new Packery('.grid', this.packeryOptions);
+    let initPositions = MainViewComponent.getSavedDraggedPositions();
 
     // init layout with saved positions
-    this.$grid.packery('initShiftLayout', initPositions, this.dataItemAttribute);
+    this.packery.initShiftLayout(initPositions, this.dataItemAttribute);
 
     // Tiles don't start in edit mode, so we make them static
     this.initDraggableItems();
@@ -53,36 +57,35 @@ export class MainViewComponent {
   }
 
   initListeners() {
+    let _this = this;
     this.$grid.on('dragItemPositioned', function () {
-      this.saveDragPositions();
+      _this.saveDragPositions();
     });
 
-    $('#edit-button').click(this.onEditButtonClicked);
+    $('#edit-button').click(function (event) {
+      _this.onEditButtonClicked(_this, event);
+    });
   }
 
-  onEditButtonClicked(event) {
-    if (this.editModeActive) {
-      this.saveDragPositions();
-      this.makeItemsStatic();
+  onEditButtonClicked(_this: any, event: any) {
+    if (_this.editModeActive) {
+      _this.makeItemsStatic();
       event.target.textContent = 'Edit';
-      this.editModeActive = false;
+      _this.editModeActive = false;
     } else {
-      this.makeItemsDraggable();
-      event.target.textContent = 'Save';
-      this.editModeActive = true;
+      _this.makeItemsDraggable();
+      event.target.textContent = 'Done';
+      _this.editModeActive = true;
     }
-    this.$grid.find('.grid-item').toggleClass('is-editable');
-  }
-
-  getSavedDraggedPositions(): string {
-    return localStorage.getItem('dragPositions');
+    _this.$grid.find('.grid-item').toggleClass('is-editable');
   }
 
   initDraggableItems() {
-    this.$grid.find('.grid-item').each(function (i, itemElem) {
-      let draggie = new Draggabilly(itemElem, this.draggabillyOptions);
-      this.$grid.packery('bindDraggabillyEvents', draggie);
-      this.draggies[i] = draggie;
+    let _this = this;
+    this.$grid.find('.grid-item').each(function (i: number, itemElem: any) {
+      let draggie = new Draggabilly(itemElem, _this.draggabillyOptions);
+      _this.packery.bindDraggabillyEvents(draggie);
+      _this.draggies[i] = draggie;
     });
   }
 
@@ -99,7 +102,7 @@ export class MainViewComponent {
   }
 
   saveDragPositions() {
-    let positions = this.$grid.packery('getShiftPositions', 'data-item-id');
+    let positions = this.packery.getShiftPositions('data-item-id');
     localStorage.setItem('dragPositions', JSON.stringify(positions));
   }
 }
