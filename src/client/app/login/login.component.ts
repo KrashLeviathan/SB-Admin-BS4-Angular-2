b5ba1994798985a788e72b8d8dc3608aa315b1dd
csteamengine <gcs@iastate.edu>
2017-03-02 15:16:54 -0600

    Got google log in to work, need to send it to the server

--- src/client/app/login/login.component.ts
+++ src/client/app/login/login.component.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 /**
-*	This class represents the lazy loaded LoginComponent.
-*/
+ *  This class represents the lazy loaded LoginComponent.
+ */
+declare const gapi: any;
 
 @Component({
-	moduleId: module.id,
-	selector: 'login-cmp',
-	templateUrl: 'login.component.html'
+  moduleId: module.id,
+  selector: 'login-cmp',
+  templateUrl: 'login.component.html'
 })
 
-export class LoginComponent { }
+export class LoginComponent {
+
+  ngAfterViewInit() {
+    gapi.signin2.render('googleBtn', {
+      'scope': 'profile email',
+      'width': 240,
+      'height': 50,
+      'longtitle': true,
+      'theme': 'dark',
+      'onsuccess': param => this.onSignIn(param)
+    });
+  }
+
+  public onSignIn(googleUser) {
+
+    let profile = googleUser.getBasicProfile();
+    console.log('Token || ' + googleUser.getAuthResponse().id_token);
+    console.log('ID: ' + profile.getId());
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail());
+    //YOUR CODE HERE
+
+  }
+
+}
+
