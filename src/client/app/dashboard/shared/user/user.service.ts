63c344fa35741f60735c22ee5397c09148353d3b
KrashLeviathan <nathankarasch@gmail.com>
2017-03-08 15:13:03 -0600

    Stubbed out EncryptionService and getter/setter for session token

--- src/client/app/dashboard/shared/user/user.service.ts
+++ src/client/app/dashboard/shared/user/user.service.ts
@@ -1,13 +1,41 @@
 import {Injectable} from '@angular/core';
 import {User} from './user';
 import {USERS} from './mock-users';
+import {EncryptionService} from '../encryption/encryption.service';
+import {Cookie} from 'ng2-cookies/ng2-cookies';
+
+export const DAYS_UNTIL_SESSION_EXPIRATION = 7;
 
 @Injectable()
 export class UserService {
-  getUser(userId: number) {
+  /**
+   * On login, the active user sessionId is encrypted and stored in the cookies.
+   * @param sessionToken
+   */
+  setActiveUserSession(sessionToken: string): void {
+    let encryptedToken = EncryptionService.encrypt(
+      EncryptionService.encode(sessionToken)
+    );
+    Cookie.set('sessionToken', encryptedToken, DAYS_UNTIL_SESSION_EXPIRATION);
+  }
+
+  getActiveUser(): Promise<User> {
+    let encryptedToken = Cookie.get('sessionToken');
+    let sessionToken = EncryptionService.decode(
+      EncryptionService.decrypt(encryptedToken)
+    );
     // TODO: Replace with HTTP request
-    return this.getUsers()
-      .then(users => users.find(user => user.userId === userId));
+    return this.getUser(1);
+  }
+
+  getUser(userId: number): Promise<User> {
+    // TODO: Replace with HTTP request
+    return new Promise(resolve => {
+      this.getUsers()
+        .then(users => {
+          resolve(users.find(user => user.userId === userId));
+        });
+    });
   }
 
   getUsers(): Promise<User[]> {
