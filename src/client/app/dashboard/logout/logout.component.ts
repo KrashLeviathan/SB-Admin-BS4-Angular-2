5da51c6dc3af490e839c3a4c1c54e75040276f99
KrashLeviathan <nathankarasch@gmail.com>
2017-04-03 11:06:23 -0500

    Logout functionality complete

--- src/client/app/dashboard/logout/logout.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  moduleId: module.id,
-  selector: 'logout-cmp',
-  templateUrl: './logout.component.html'
-})
-
-export class LogoutComponent { }
