794fbf4f7f1d1074dea7a13e6f4cd82ba36db491
KrashLeviathan <nathankarasch@gmail.com>
2017-04-03 00:41:49 -0500

    UserService refactor part 2

--- src/client/app/shared/user/user.service.ts
+++ src/client/app/shared/user/user.service.ts
@@ -6,7 +6,6 @@ import {Headers, Http, RequestOptions, Response} from '@angular/http';
 import {UserPreferences} from './user-preferences';
 import {AlertType, PopoverControllerComponent} from '../popover-controller/popover-controller';
 
-
 export const DAYS_UNTIL_SESSION_EXPIRATION = 7;
 
 @Injectable()
@@ -28,11 +27,20 @@ export class UserService {
     //TODO send sessionToken to the server to save.
   }
 
-  setActiveUser(user: User) {
+  /**
+   * Caches the currently logged in user and updates the Cookie.
+   * @param user
+   */
+  setActiveUser(user: User): void {
     Cookie.set('userId', user.userId.toString());
     UserService.activeUser = user;
   }
 
+  /**
+   * Returns the user who is logged in and actively using SmartSync.
+   * Caches the value for additional accesses.
+   * @returns {Promise<User>}
+   */
   getActiveUser(): Promise<User> {
     // TODO: We should be storing the
     // let encryptedToken = Cookie.get('sessionToken');
@@ -58,41 +66,39 @@ export class UserService {
     });
   }
 
+  /**
+   * Gets the User with the given userId.
+   * On failure, it displays the error message popover and returns null.
+   * @param userId
+   * @returns {Promise<User>}
+   */
   getUser(userId: number): Promise<User> {
     return new Promise(resolve => {
       Promise.all([
         this.http.get(`http://localhost:8000/users/` + userId).toPromise(),
         this.getUserHousehold(userId),
         this.getUserPreferences(userId)
-      ]).then(
-        response => {
-
-          let body = response[0].json();
-          let user = new User();
-          user.userId = body.userId;
-          user.email = body.email;
-          user.givenName = body.givenName;
-          user.familyName = body.familyName;
-          user.imageURL = body.imageURL;
-          user.role = body.role;
-          user.householdId = response[1];
-          user.preferences = response[2];
-          user.isAdmin = (user.role == '1');
-          resolve(user);
-        }
-      )
-        .catch(response => {
-          this.handleError(response);
-          resolve(null);
-        });
+      ]).then(responseArray => {
+        let partialUser = responseArray[0].json();
+        let user = this.createUser(partialUser, responseArray[1], responseArray[2]);
+        resolve(user);
+      }).catch(response => {
+        this.handleError(response);
+        resolve(null);
+      });
     });
-
   }
 
   getUserByGoogle(googleId: string): Promise<Response> {
     return this.http.get(`http://localhost:8000/users/google/` + googleId).toPromise();
   }
 
+  /**
+   * Returns the userId for the given email address.
+   * On failure, it displays the error message popover and returns 0.
+   * @param email
+   * @returns {Promise<number>}
+   */
   getUserIdByEmail(email: string): Promise<number> {
     return new Promise(resolve => {
       let encodedEmail = encodeURIComponent(email);
@@ -111,30 +117,24 @@ export class UserService {
     return this.http.post('http://localhost:8000/users/', JSON.stringify(myUser), options).toPromise();
   }
 
+  /**
+   * Returns an array of Users from the given householdId. The User objects
+   * aren't completely built, however (not loading UserPreferences for each one).
+   * On failure, it displays the error message popover and returns null.
+   * @param householdId
+   * @returns {Promise<User[]>}
+   */
   getUsers(householdId: number): Promise<User[]> {
     return new Promise(resolve => {
       this.http.get(`http://localhost:8000/households/` + householdId + `/users`).toPromise()
-        .then(
-          response => {
-            let users = new Array<User>();
-            let body = response.json();
-            let count = 0;
-            body.forEach((currIndex: any) => {
-              let user = new User();
-              user.userId = currIndex.userId;
-              user.email = currIndex.email;
-              user.givenName = currIndex.givenName;
-              user.familyName = currIndex.familyName;
-              user.imageURL = currIndex.imageURL;
-              user.role = currIndex.role;
-              user.displayName = currIndex.displayName;
-              user.isAdmin = (user.role == '1');
-              users[count] = user;
-              count++;
-            });
-            resolve(users);
-          }
-        )
+        .then(response => {
+          let users: User[] = [];
+          let partialUsers = response.json();
+          partialUsers.forEach((partialUser: any) => {
+            users.push(this.createUser(partialUser, householdId, null));
+          });
+          resolve(users);
+        })
         .catch(response => {
           this.handleError(response);
           resolve(null);
@@ -142,26 +142,52 @@ export class UserService {
     });
   }
 
+  /**
+   * Returns the householdId for the given userId.
+   * On failure, it displays the error message popover and returns null.
+   * @param userId
+   * @returns {Promise<number>}
+   */
   getUserHousehold(userId: number): Promise<number> {
     return new Promise(resolve => {
       this.http.get(`http://localhost:8000/users/` + userId + `/household`).toPromise()
         .then(household => {
           let body = household.json();
           resolve(body.householdId);
+        })
+        .catch(response => {
+          this.handleError(response);
+          resolve(null);
         });
     });
   }
 
+  /**
+   * Returns the householdId for the given user (via user's googleId).
+   * On failure, it displays the error message popover and returns null.
+   * @param googleId
+   * @returns {Promise<number>}
+   */
   getUserHouseholdByGoogle(googleId: string): Promise<number> {
     return new Promise(resolve => {
       this.http.get(`http://localhost:8000/users/google/` + googleId + `/household`).toPromise()
-        .then(household => {
-          let body = household.json();
-          resolve(body.householdId);
+        .then(response => {
+          let household = response.json();
+          resolve(household.householdId);
+        })
+        .catch(response => {
+          this.handleError(response);
+          resolve(null);
         });
     });
   }
 
+  /**
+   * Gets the UserPreferences for the given userId.
+   * On failure, it displays the error message popover and returns null.
+   * @param userId
+   * @returns {Promise<UserPreferences>}
+   */
   getUserPreferences(userId: number): Promise<UserPreferences> {
     //TODO: Currently just getting drag positions
     //TODO set the user colorScheme
@@ -179,6 +205,13 @@ export class UserService {
     });
   }
 
+  /**
+   * Sets the UserPreferences for the given userId. Returns true on success.
+   * On failure, it displays the error message popover and returns false.
+   * @param userId
+   * @param prefs
+   * @returns {Promise<boolean>}
+   */
   setUserPreferences(userId: number, prefs: any): Promise<boolean> {
     // TODO: Replace with HTTP request
     localStorage.setItem('dragPositions', JSON.stringify(prefs));
@@ -190,6 +223,12 @@ export class UserService {
     });
   }
 
+  /**
+   * Saves user settings from the UserSettings page form. Returns true on success.
+   * On failure, it displays the error message popover and returns false.
+   * @param formData
+   * @returns {Promise<boolean>}
+   */
   saveUser(formData: Object): Promise<boolean> {
     // TODO: Save form data to server
     console.log(formData);
@@ -199,24 +238,16 @@ export class UserService {
     });
   }
 
+  /**
+   * Deletes the given user. Returns true on success.
+   * On failure, it displays the error message popover and returns false.
+   * @param userId
+   * @returns {Promise<boolean>}
+   */
   deleteUser(userId: number): Promise<boolean> {
-    console.log('deleteUser(' + userId + ') --> success');
     return new Promise(resolve => {
       this.http.delete(`http://localhost:8000/users/` + userId).toPromise()
-        .then(
-          response => {
-            let body = response.json();
-            let user = new User();
-            user.userId = body.userId;
-            user.email = body.email;
-            user.givenName = body.givenName;
-            user.familyName = body.familyName;
-            user.imageURL = body.imageURL;
-            user.role = body.role;
-
-            resolve(user);
-          }
-        )
+        .then(() => resolve(true))
         .catch(response => {
           this.handleError(response);
           resolve(false);
@@ -224,18 +255,21 @@ export class UserService {
     });
   }
 
+  /**
+   * Makes the user an admin. Returns true on success.
+   * On failure, it displays the error message popover and returns false.
+   * @param userId
+   * @returns {Promise<boolean>}
+   */
   giveAdminPrivileges(userId: number): Promise<boolean> {
     return new Promise(resolve => {
       let json = new User();
-      json.userId = userId;
-      json.role = '1';
+      json.userId = userId; // the user being given privileges
+      json.role = '1';      // tells the server an admin is making the request
+      // TODO: The security on this part should be improved...
 
       this.http.post(`http://localhost:8000/users/createAdmin`, JSON.stringify(json), this.jsonHeader()).toPromise()
-        .then(
-          response => {
-            resolve(true);
-          }
-        )
+        .then(() => resolve(true))
         .catch(response => {
           this.handleError(response);
           resolve(false);
@@ -243,27 +277,21 @@ export class UserService {
     });
   }
 
+  /**
+   * Removes admin privileges from the user. Returns true on success.
+   * On failure, it displays the error message popover and returns false.
+   * @param userId
+   * @returns {Promise<boolean>}
+   */
   revokeAdminPrivileges(userId: number): Promise<boolean> {
     return new Promise(resolve => {
       let json = new User();
-      json.userId = userId;
-      json.role = '1';
+      json.userId = userId; // the user being given privileges
+      json.role = '1';      // tells the server an admin is making the request
+      // TODO: The security on this part should be improved...
 
       this.http.post(`http://localhost:8000/users/removeAdmin`, JSON.stringify(json), this.jsonHeader()).toPromise()
-        .then(
-          response => {
-            let body = response.json();
-            let user = new User();
-            user.userId = body.userId;
-            user.email = body.email;
-            user.givenName = body.givenName;
-            user.familyName = body.familyName;
-            user.imageURL = body.imageURL;
-            user.role = body.role;
-
-            resolve(true);
-          }
-        )
+        .then(() => resolve(true))
         .catch(response => {
           this.handleError(response);
           resolve(false);
@@ -271,6 +299,15 @@ export class UserService {
     });
   }
 
+  /**
+   * Creates an invite for the user connected to the given email.
+   * First it will check if a user with the given email is in the system.
+   * If and only if such a user exists, the invite will be created for
+   * that user to join the activeUser's household. Returns true on success.
+   * On failure, it displays the error message popover and returns false.
+   * @param email
+   * @returns {Promise<T>}
+   */
   inviteUserEmail(email: string): Promise<boolean> {
     return new Promise(resolve => {
       this.getUserIdByEmail(email).then(userId => {
@@ -284,13 +321,11 @@ export class UserService {
           };
 
           this.http.post(`http://localhost:8000/invites`, JSON.stringify(json), this.jsonHeader()).toPromise()
-            .then(response => {
-              // Invite was successful
-              resolve(true);
-            }).catch(response => {
-            this.handleError(response);
-            resolve(false);
-          });
+            .then(() => resolve(true))
+            .catch(response => {
+              this.handleError(response);
+              resolve(false);
+            });
         }
       }).catch(response => {
         this.handleError(response);
@@ -299,12 +334,14 @@ export class UserService {
     });
   }
 
+  // Possible cruft to clean up later...
   // private extractData(res: Response) {
   //   let body = res.json();
   //   console.log(body.data);
   //   return body.data || {};
   // }
 
+  // Displays a popover with the error message.
   private handleError(error: Response | any) {
     let errMsg: string;
     if (error instanceof Response) {
@@ -323,8 +360,29 @@ export class UserService {
     PopoverControllerComponent.createAlert(AlertType.DANGER, errMsg, 60000);
   }
 
+  // The header attached to POST requests sending json data
   private jsonHeader(): RequestOptions {
     let headers = new Headers({'Content-Type': 'application/json'});
     return new RequestOptions({headers: headers});
+  }
+
+  // Creates a full User object from a partial user object (such is returned in GET requests)
+  private createUser(partialUser: any, householdId: number, preferences: UserPreferences): User {
+    return {
+      userId: partialUser.userId || 0,
+      googleId: partialUser.googleId || '',
+      displayName: partialUser.displayName || '',
+      fullName: partialUser.fullName || '',
+      givenName: partialUser.givenName || '',
+      familyName: partialUser.familyName || '',
+      imageURL: partialUser.imageURL || '',
+      email: partialUser.email || '',
+      householdId: householdId,
+      role: partialUser.role || '',
+      created: partialUser.created || '',
+      lastUpdated: partialUser.lastUpdated || '',
+      isAdmin: (partialUser.role == '1') || false,
+      preferences: preferences
+    };
   }
 }
